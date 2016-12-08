b7bf3a565c4f41573bc91ff2a0d57744a003b72d
Ben Christians <bbc7909@rit.edu>
2016-12-07 19:23:04 -0500

    Fully implemented all currently planned functionality for the AvailabilityMap Class. Also pushed the .idea file directory.

--- ShiftScheduler/Employee.py
+++ ShiftScheduler/Employee.py
@@ -1,22 +1,14 @@
 from ShiftScheduler import ShiftTime
 
-class Employee():
-    """
-    A Class to represent each employee in the schedule
-    """
-    name = "John Doe"
-    seniority = False;
-
-    def __init__(self, name, seniority):
-        self.name = name
-        self.seniority = seniority
-
-
 class AvailabilityMap():
     """
     A class to represent each employee's availability
     """
     availability_times = {}
+
+    def __str__(self):
+        # TODO
+        return "asd"
 
     def add_time(self, time):
         """
@@ -25,8 +17,45 @@ class AvailabilityMap():
         :return: true if the time was added, false if it was
         already in the map
         """
-        # TODO
+        # TODO Test
         assert type(time) is ShiftTime.ShiftTime
 
-        self.availability_times.update()
+        # If the employee is already available at time return false
+        # to signify no changes made
+        if self.availability_times.get(time):
+            return False
 
+        self.availability_times.update({time: True})
+        return True
+
+    def get_total_hours(self):
+        """
+        Determines the total hours the employee is available
+        :return: a double representing the total hours an employee
+        is available
+        """
+        # TODO Test
+        hours = 0
+        for key in self.availability_times.keys():
+            if self.availability_times.get(key):
+                hours += 0.5
+        return hours
+
+class Employee():
+    """
+    A Class to represent each employee in the schedule
+    """
+    name = "Anonymous Employee"
+    seniority = False;
+    available_times = AvailabilityMap()
+    preffered_times = AvailabilityMap()
+
+
+    def __init__(self, name, seniority):
+        self.name = name
+        self.seniority = seniority
+
+if __name__ == "__main__":
+    bobby = Employee("Bobby Smith", True)
+
+    print(str(bobby.available_times))
